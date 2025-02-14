1ba4bdf027e79b5f7a3737a1bb8e0e8df679f39d
Anselme Goetschmann <anselme.goetschmann@gmail.com>
2025-02-14 11:15:52 +0100

    spring initializr

---
+++ src/main/java/com/example/spring_vue_graphql/SpringVueGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_vue_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueGraphqlApplication.class, args);
+	}
+
+}
